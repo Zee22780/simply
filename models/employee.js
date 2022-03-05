8f5a309baba96bb081af1aed90b08c6ac80e4e5b
zuri227 <zuri22780@gmail.com>
2022-03-05 15:17:06 -0500

    Create employee model

---
+++ models/employee.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const employeeSchema = new Schema({
+  eName: String,
+  startDate: Date,
+  title: String,
+  department: String,
+  location: String,
+  mName: String,
+  eId: {type: Schema.Types.ObjectId, ref: "Profile"}
+})
+
+const Employee = mongoose.model('Employee', employeeSchema)
+
+export {
+  Employee
+}
