4024629d1b0f4c955d4560197e7518fc83f6f497
zuri227 <zuri22780@gmail.com>
2022-03-06 13:01:29 -0500

    Display all employees who have been added to All Employees page

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,9 +1,36 @@
 import { Employee } from "../models/employee.js"
 
+function index(req, res) {
+  Employee.find({})
+  .then(employees => {
+    res.render('employees/index', {
+      employees,
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/employees')
+  })
+}
+
 function newEmployee(req, res) {
   res.render('employees/new')
 }
 
+function create(req, res) {
+  req.body.owner = req.user.profile._id
+  Employee.create(req.body)
+  .then(employee => {
+    res.redirect('/employees')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/employees')
+  })
+}
+
 export {
+  index,
   newEmployee as new,
+  create,
 }
--- routes/employees.js
+++ routes/employees.js
@@ -1,10 +1,17 @@
 import { Router } from 'express'
 import * as employeesCtrl from "../controllers/employees.js"
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
+// GET localhost:3000/employees
+router.get('/', employeesCtrl.index)
+
 // GET localhost:3000/employees/new
 router.get('/new', employeesCtrl.new)
+
+// POST localhost:3000/employees
+router.post("/", isLoggedIn, employeesCtrl.create)
 
 export {
   router
