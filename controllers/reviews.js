50eccb7d1d8bfabf74936fdaba7ec9e0cd40efc5
zuri227 <zuri22780@gmail.com>
2022-03-13 12:30:21 -0400

    Remove test console log from newReview controller function

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -5,9 +5,7 @@ function newReview(req, res) {
   console.log(req.user)
   Profile.findById(req.user.profile._id).populate('employees')
     .then(self => {
-      
       const isSelf = self._id.equals(req.user.profile._id)
-      console.log('hello zuri', self)
       res.render("reviews/new", {
         self,
         isSelf,
